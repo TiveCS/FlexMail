370fb963dc90bbdd2ed9997fe902dfe7af7c77da
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-31 10:32:30 +0700

    initial commit

---
+++ src/main/java/io/github/tivecs/flexmail/FlexMail.java
@@ -0,0 +1,17 @@
+package io.github.tivecs.flexmail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlexMail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
